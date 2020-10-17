fd470425a1ce447b94114d8d3e2e98b55dc8c136
ankitpandey0 <61089663+ankitpandey0@users.noreply.github.com>
2020-10-17 23:00:13 +0530

    Update Galton_board_solution.py

--- Course2_scripts/Galton_board_solution.py
+++ Course2_scripts/Galton_board_solution.py
@@ -11,19 +11,19 @@ So THE CONCENTRATION of beads in bins is just the sum of all elements times 100
 def main(n, start, stop):
     fractions = []
     #iterate through all favourable values of k
-    for k in range(start, stop+1):
+    for i in range(start, stop+1):
         #calculate the fraction
-        fract = choose(n, k)/2**n
+        fract = choose(n, i)/2**n
         fractions.append(fract)
 
         #uncomment the following line to get a more verbose output
-        #print(f"TRACKING- k:{k} _______ fraction={fract}")
+        #print(f"TRACKING- i:{i} _______ fraction={fract}")
     #calculate and print the concentration
     print("Concentration: ",sum(fractions)*100)
 
 if __name__ == "__main__":
     n = int(input("Enter layers on board:"))-1
-    print("Enter the start and stop point of values of k:-")
+    print("Enter the start and stop point of values of i:-")
     start = int(input("(K) start point:"))
     stop = int(input("(K) end point:"))
     main(n, start, stop)
